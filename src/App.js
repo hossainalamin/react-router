6390a66ca072e5cbe17cd4af5f7fd19c09ab7cd0
hossainalamin <hossainalamin980@gmail.com>
2023-01-03 23:03:31 +0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Card, CardGroup } from 'react-bootstrap';
+import { AccordionButton, Button, Card, CardGroup } from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
+import { Route, Routes } from 'react-router';
+import About from './components/About/About.js';
+import Contact from './components/Contact/Contact.js';
+import Friend from './components/Friend/Friend.js';
+import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
@@ -11,6 +16,12 @@ function App() {
       <Spinner animation="border" role="status">
       <span className="visually-hidden">Loading...</span>
     </Spinner>
+    <Routes>
+      <Route path='/' element={<About></About>}></Route>
+      <Route path='/contact' element={<Contact></Contact>}></Route>
+      <Route path='/friend' element={<Friend></Friend>}></Route>
+      <Route path='*' element={<NotFound></NotFound>}></Route>
+    </Routes>
    
     </div>
   );
---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friend = () => {
+    return (
+        <div>
+            <h1>Hello Friend</h1>
+        </div>
+    );
+};
+
+export default Friend;
